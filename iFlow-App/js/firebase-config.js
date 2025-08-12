c2dff89d677a13deb83d7ecff67942514a856ba4
iflowpmb-del <iflowpmb@gmail.com>
2025-08-12 16:41:40 -0300

    v1.0.1

--- iFlow-App/js/firebase-config.js
+++ iFlow-App/js/firebase-config.js
@@ -5,12 +5,13 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.6.1/firebase
 // ¡IMPORTANTE!
 // Estas credenciales deben ser las de tu proyecto en la consola de Firebase.
 const firebaseConfig = {
-  apiKey: 'AIzaSyDkvzx0qjJUW-mrsECP6RWEjTPzeyiRDU0',
-  authDomain: 'ifloepmb-desarrollo.firebaseapp.com',
-  projectId: 'ifloepmb-desarrollo',
-  storageBucket: 'ifloepmb-desarrollo.firebasestorage.app',
-  messagingSenderId: '785718569438',
-  appId: '1:785718569438:web:e00df90c0cd29df4d6d54f',
+  apiKey: 'AIzaSyBgwTgqaseQEFVLnpHdURN7XnVCTC1DAYc',
+  authDomain: 'iflowapp-94b0b.firebaseapp.com',
+  projectId: 'iflowapp-94b0b',
+  storageBucket: 'iflowapp-94b0b.firebasestorage.app',
+  messagingSenderId: '408374534171',
+  appId: '1:408374534171:web:0b4050515574c6f40c98be',
+  measurementId: 'G-TK91KK97M2',
 };
 
 const app = initializeApp(firebaseConfig);
